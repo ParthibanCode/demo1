eea570bda7353b02f1dcda4105ca5d0114331494
ParthibanCode <parthlearningg@gmail.com>
2022-01-18 15:03:37 +0530

    Added deleteUser method

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,4 +21,9 @@ public class UserController {
 		System.out.println(user.getName());
 	}
 
+	@DeleteMapping("/user")
+	void deleteUser() {
+		
+	}
+	
 }
